3ac3ed3cbc1e4ef99d58e3d28cc14ce1e1b4440d
Gabriel Pedro Nogueira <gabrielpedronogueira@gmail.com>
2021-12-23 08:19:42 -0300

    Projeto criado

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,38 @@
+package entities;
+
+public class ImportedProduct extends Product {
+
+	private Double customsFee;
+	
+	public ImportedProduct() {
+	}
+	
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+	
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+	
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return name
+				+ " $ " 
+				+ String.format("%.2f", price)
+				+ " (Customs fee:"
+				+ " $ "
+				+ customsFee
+				+")";
+	}
+	
+	public Double totalPrice() {
+		return getPrice() + customsFee;
+	}
+	
+}
